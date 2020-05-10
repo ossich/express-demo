d02874b3d20bf7dc409a1e50a3b525cf77822d68
ossich <andrei.goldman@gmail.com>
2020-05-10 13:07:07 +0000

    EnvironmentVar + NodeMon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req,res) => {
 
-    res.send('Hello World!...');
+    res.send('Hello World! 001...');
 
 });
 
@@ -13,5 +13,5 @@ app.get('/api/courses', (req,res) => {
 
 });
 
-
-app.listen(3000, () => console.log('Listening on port 3000!'));
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log(`Listening on port ${port}...`));
