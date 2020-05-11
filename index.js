05795fcbacba1540d86112ef6a83dc9283113bca
ossich <andrei.goldman@gmail.com>
2020-05-11 08:40:50 +0000

    Handling HTTP Get request

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-
+const courses = [
+    { id : 1, name : 'course1' },
+    { id : 2, name : 'course2' },
+    { id : 3, name : 'course3' },
+    { id : 4, name : 'course4' },
+];
 app.get('/', (req,res) => {
 
     res.send('Hello World! 001...');
@@ -9,13 +14,17 @@ app.get('/', (req,res) => {
 
 app.get('/api/courses', (req,res) => {
 
-    res.send([1,2,3]);
+    res.send(courses);
 
 });
 
-app.get('/api/posts/:year/:month', (req,res) => {
+app.get('/api/courses/:id', (req,res) => {
 
-    res.send(req.query);
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course whith the given ID is not found.'); // 404
+   res.send(course);
+
+ // res.send(req.query);
  // res.send(req.params);
 
 });
